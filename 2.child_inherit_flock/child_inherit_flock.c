2173dbb8407719be014bba716b3879a3999b155e
Gao Feng <fgao@ikuai8.com>
2016-08-26 22:43:20 +0800

    2.child_inherit_flock: Add the child process inherits parent's flock

---
+++ 2.child_inherit_flock/child_inherit_flock.c
@@ -0,0 +1,58 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  child_inherit_flock.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  08/26/2016 10:01:43 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <sys/file.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(void)
+{
+	int fd = open("./test_flock.txt", O_RDWR|O_CREAT, 0644);
+	if (fd == -1) {
+		printf("Fail to open file\n");
+		exit(1);
+	}
+
+	int ret = flock(fd, LOCK_EX);
+	if (ret == -1) {
+		printf("Fail to flock\n");
+		exit(1);
+	}
+	printf("Parent get the flock at the first time\n");
+
+	pid_t child = fork();
+	if (child == -1) {
+		printf("Fail to fork\n");
+		exit(1);
+	} else if (child == 0) {
+		printf("Child sleeps now\n");
+		sleep(30);
+		printf("Child wakes up and exits\n");
+		exit(1);
+	}
+
+	printf("Parent exit\n");
+
+	return 0;
+}
+
